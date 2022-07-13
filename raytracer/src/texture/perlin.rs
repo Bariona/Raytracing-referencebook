c0f8cbcbd401ba40fbc4c09362714008189e7e42
Quanquan Peng <bariona@qq.com>
2022-07-13 10:36:45 +0800

    add perlin 三线性插值

--- raytracer/src/texture/perlin.rs
+++ raytracer/src/texture/perlin.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::many_single_char_names)]
+use ndarray::Array3;
 use rand::{thread_rng, Rng};
 
 use crate::Hit::{random_double, Color, Point3};
@@ -25,14 +27,6 @@ impl Perlin {
         let perm_y = Perlin::perlin_generate_perm();
         let perm_z = Perlin::perlin_generate_perm();
 
-        // let mut cnt = 0;
-        // for i in 0..POINT_COUNT {
-        //     if perm_x[i] == perm_y[i] {
-        //         cnt += 1;
-        //     }
-        // }
-        // println!("{}", cnt);
-
         Self {
             perm_x,
             perm_y,
@@ -41,14 +35,48 @@ impl Perlin {
         }
     }
 
-    pub fn noise(&self, p: &Point3) -> f64 {
-        // hash: get a double \in [0, 1)
-        let i = (4. * p.x().abs()) as usize & 255;
-        let j = (4. * p.y().abs()) as usize & 255;
-        let k = (4. * p.z().abs()) as usize & 255;
+    fn trilinear_inerp(a3: Array3<f64>, u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.;
 
-        // 对于一个固定的p, 一定要返回固定的noise, 不然就是全一个基调的灰色(白色+黑色的混合结果)
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1. - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1. - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1. - w))
+                        * a3[[i, j, k]];
+                }
+            }
+        }
+        accum
+    }
+
+    pub fn noise(&self, p: &Point3) -> f64 {
+        let mut u = (p.x() - p.x().floor()).abs();
+        let mut v = (p.y() - p.y().floor()).abs();
+        let mut w = (p.z() - p.z().floor()).abs();
+
+        u = u.powi(2) * (3. - 2. * u);
+        v = v.powi(2) * (3. - 2. * v);
+        w = w.powi(2) * (3. - 2. * w);
+
+        let i = p.x().floor() as isize;
+        let j = p.y().floor() as isize;
+        let k = p.z().floor() as isize;
+
+        let mut a3 = Array3::<f64>::zeros((2, 2, 2));
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    a3[[di, dj, dk]] = self.ranfloat[self.perm_x
+                        [((i + di as isize) & 255) as usize]
+                        ^ self.perm_y[((j + dj as isize) & 255) as usize]
+                        ^ self.perm_z[((k + dk as isize) & 255) as usize]];
+                }
+            }
+        }
+        Self::trilinear_inerp(a3, u, v, w)
     }
 
     pub fn perlin_generate_perm() -> Vec<usize> {
